3bdc65b6134b9709a56a856080d181450306d694
DistnGeorisPXL <distn.georis@student.pxl.be>
2024-10-04 10:04:03 +0200

    initial commit

---
+++ oefening-WPL-maandag/oefening-WPL-maandag/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace oefening_WPL_maandag
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
