fb674717416a02eab83a8cc9c5a4ae623ae5cd7e
DistnGeorisPXL <distn.georis@student.pxl.be>
2024-11-17 20:07:30 +0100

    .

---
+++ rogoef1/rogoef1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace rogoef1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void redBtn_Click(object sender, RoutedEventArgs e)
+        {
+            redImage.Visibility = Visibility.Visible;
+
+
+        }
+
+        private void orangeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            orangeImage.Visibility = Visibility.Visible;
+        }
+
+        private void greenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            greenImage.Visibility=Visibility.Visible;
+        }
+    }
+}
---
+++ wisselgeldcalculator/wisselgeldcalculator/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace wisselgeldcalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        double amountToPayBack;
+        double amountToPayBackTimes100;
+
+        private void calculateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(totalAmountToPayTextBox.Text) && double.TryParse(totalAmountToPayTextBox.Text, out double amountToPayInput) && !string.IsNullOrEmpty(totalAmountReceivedTextBox.Text) && double.TryParse(totalAmountReceivedTextBox.Text, out double amountReceivedInput))
+            {
+                amountToPayBack = amountReceivedInput - amountToPayInput;
+                amountToPayBackTimes100 = amountToPayBack * 100;
+
+
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Het gegeven en/of gevraagde bedrag is verkeerd of niet ingevuld.", "FOUT!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+        }
+    }
+}
